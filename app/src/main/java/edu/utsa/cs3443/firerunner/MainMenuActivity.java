b8c74d199eebb7f513fc29db472652f8d9b319d0
digs130502 <113381944+digs130502@users.noreply.github.com>
2023-11-16 15:40:28 -0600

    added and modified stuff

--- app/src/main/java/edu/utsa/cs3443/firerunner/MainMenuActivity.java
+++ app/src/main/java/edu/utsa/cs3443/firerunner/MainMenuActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -19,18 +20,20 @@ public class MainMenuActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View view) {
 
-        Button clickedButton = (Button) view;
-        String buttonText = clickedButton.getText().toString().toLowerCase();
+        TextView clickedButton = (TextView) view;
+        String buttonText = clickedButton.getText().toString().toLowerCase(); /*
         if(buttonText.equalsIgnoreCase("start game")){
             Intent gameIntent = new Intent(MainMenuActivity.this, GameActivity.class);
-            startActivity(GameActivity);
+            startActivity(gameIntent);
         }else if (buttonText.equalsIgnoreCase("settings")){
             Intent settingsIntent = new Intent(MainMenuActivity.this, SettingsActivity.class);
-            startActivity(SettingsActivity);
-        }else if (buttonText.equalsIgnoreCase("quit game")) {
-            Intent quitIntent = new Intent(MainMenuActivity.this, QuitActivity.class);
-            startActivity(QuitActivity);
+            startActivity(settingsIntent);
+        }*/
+        if (buttonText.equalsIgnoreCase("quit game")) {
+            finish(); //Finish the current activity
+            System.exit(0); //Terminate the app's process
         }
+
     }
 
     private void setupButtons() {
@@ -38,7 +41,7 @@ public class MainMenuActivity extends AppCompatActivity implements View.OnClickL
         int[] buttonIDs = {R.id.start_button, R.id.settings_button, R.id.quit_button};
         String[] buttonText = {"Start Game", "Settings", "Quit Game"};
         for(int i = 0; i < buttonIDs.length; i++){
-            Button button = findViewById(buttonIDs[i]);
+            TextView button = findViewById(buttonIDs[i]);
             button.setText(buttonText[i]);
             button.setOnClickListener(this);
         }
